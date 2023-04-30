2e59a2039152607c7d4c7b7c812e320cb1f0859e
Jithu Prabhakar <join.jitz@gmail.com>
2023-04-30 15:59:29 +0530

    'fronend sample model completed'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+import { Container } from 'react-bootstrap'
+
+import Home from './pages/Home'
+import SingleProduct from './pages/SingleProduct'
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome to Proshop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/product/:id' element={<SingleProduct />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
