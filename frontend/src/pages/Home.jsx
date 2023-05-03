6325171dd810a1fdfe684a6969afef5cfabe85cb
Jithu Prabhakar <join.jitz@gmail.com>
2023-05-03 20:26:36 +0530

    schema completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
-import products from '../products'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest Products</h1>
--- frontend/src/pages/SingleProduct.jsx
+++ frontend/src/pages/SingleProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Col,
   Image,
@@ -10,11 +10,24 @@ import {
 } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const SingleProduct = () => {
   const { id } = useParams()
-  const product = products.find((p) => p._id === id)
+
+  const [product, setProduct] = useState([])
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [id])
+
+  // const product = products.find((p) => p._id === id)
 
   return (
     <>
